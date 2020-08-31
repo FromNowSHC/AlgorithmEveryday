1575a4c11069bb5bcef895b9e0d11bc3aad032ce
SHC-MyAccount <sunshcchao@163.com>
2020-08-31 18:15:03 +0800

    树的深度

---
+++ src/easy/DepthTree.java
@@ -0,0 +1,33 @@
+package easy;
+
+/**
+ * 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ *
+ * @author shc
+ * @date 2020-08-31
+ */
+public class DepthTree {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
